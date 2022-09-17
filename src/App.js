36a4dd40993e1b0f96d81d621600d6a959ad0064
Hemanth <hemanthsingh11011@gmail.com>
2022-09-17 22:10:43 +0530

    Add FAQs and Help Icon

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Navbar from './components/Navbar/Navbar';
 import CoverPage from './components/CoverPage/CoverPage';
 import EventCarousel from './components/EventCarousel/EventCarousel';
 import {EventData} from './components/EventCarousel/EventData';
-import Timeline from './components/Timeline/Timeline'
+import Timeline from './components/Timeline/Timeline';
+import FAQs from './components/FAQs/FAQs';
+import Help from './components/HelpIcon/Help';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <CoverPage/>
       <EventCarousel slides={EventData}/>
       <Timeline/>
+      <FAQs/>
+      <Help/>
     </ChakraProvider>
   );
 }
--- src/components/FAQs/FAQs.js
+++ src/components/FAQs/FAQs.js
@@ -1,8 +1,117 @@
 import React from 'react'
-
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+    Box
+  } from '@chakra-ui/react'
 function FAQs() {
   return (
-    <div>FAQs</div>
+    <div><Accordion allowToggle ml={150} mr={150}>
+    <AccordionItem>
+      <h2>
+        <AccordionButton _expanded={{ bg: 'tomato', color: 'white' }}>
+          <Box flex='1' textAlign='left'>
+            FAQ 1
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={4}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </AccordionPanel>
+    </AccordionItem>
+  
+    <AccordionItem>
+      <h2>
+        <AccordionButton _expanded={{ bg: 'tomato', color: 'white' }}>
+          <Box flex='1' textAlign='left'>
+            FAQ 2
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={4}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </AccordionPanel>
+    </AccordionItem>
+
+    <AccordionItem>
+      <h2>
+        <AccordionButton _expanded={{ bg: 'tomato', color: 'white' }}>
+          <Box flex='1' textAlign='left'>
+            FAQ 3
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={4}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </AccordionPanel>
+    </AccordionItem>
+
+    <AccordionItem>
+      <h2>
+        <AccordionButton _expanded={{ bg: 'tomato', color: 'white' }}>
+          <Box flex='1' textAlign='left'>
+            FAQ 4
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={4}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </AccordionPanel>
+    </AccordionItem>
+
+    <AccordionItem>
+      <h2>
+        <AccordionButton _expanded={{ bg: 'tomato', color: 'white' }}>
+          <Box flex='1' textAlign='left'>
+            FAQ 5   
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={4}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </AccordionPanel>
+    </AccordionItem>
+
+    <AccordionItem>
+      <h2>
+        <AccordionButton _expanded={{ bg: 'tomato', color: 'white' }}>
+          <Box flex='1' textAlign='left'>
+            FAQ 6
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={4}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </AccordionPanel>
+    </AccordionItem>
+  </Accordion></div>
   )
 }
 
