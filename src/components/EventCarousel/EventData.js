14741a4e947ba3aa1c4b0c6a0686ce8d400acb48
aditansh <aadityamahanta07@gmail.com>
2022-09-27 16:32:59 +0530

    add timeline, faq and details

--- src/components/EventCarousel/EventData.js
+++ src/components/EventCarousel/EventData.js
@@ -1,23 +1,70 @@
 export const EventData = [
     {
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."  
+      heading: "Rules",
+      text:`For smooth sailing in any endeavor, a set of guidelines must be followed. While Code 2 Clone encourages you to think outside the box and break norms, the competitors are requested to follow the given points for the duration of the event.
+      
+      1. The contest is open for all, and there is no academic eligibility requirement.
+      
+      2. You can refer to any online learning resource, tutorials, or relevant HTML and CSS documentation for assistance during your coding rounds.
+      
+      3. Your code will be auto saved and submitted at the end of each round, on the basis of which your performance will be judged.
+       
+      4. You cannot communicate with your fellow competitors or your friends in any way, either through online or offline means.
+      
+      5. If you are found to be in violation of any of the rules, the organisers reserve the right to disqualify you immediately.
+      
+      6. During the event, if you face any difficulty technical or otherwise, feel free to reach out to any of our representatives present.`
     },
     {
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."  
+      heading: "Round 1",
+      text:`The first challenge you face in Code 2 Clone begins here.
+      
+      Your task will be to replicate the given images in your coding sandbox.
+      
+      There will be 2 questions in the first round, and you can only move on to the next after finishing the first.
+      
+      Neither question are choice based and both carry weightage in the final assessment of the participant's performance.
+      
+      The duration of this round will be about 60 minutes. 
+      
+      Upon conclusion your auto submitted code will be sent to the judges for evaluation, and the results for Round 1 will be shortly announced.` 
     },
     {
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."  
+      heading:"Round 2",
+      text:`Half way there, and a bigger beast to slay this time. As you progress, your obstacles get tougher and you really get tested!
+
+      The qualified coders of Round 1 will compete here.
+      
+      Unlike the first round, only one image will be displayed and that will be the common evaluation for all, albeit a little more challenging than before.
+      
+      The duration of this round will also be about 60 minutes and the code will be auto submitted at the end.
+      
+      The participants will be informed of the qualifiers of the last round shortly after completion.` 
     },
     {
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."  
+      heading:"Round 3",
+      text:`The final stretch is here. All your grit and intellect boils down to this one last challenge.
+      Beyond this your prize awaits!
+      
+      The final round will be a showdown between the cream of the crop!
+      
+      You will get only one image to replicate, similar as round 2, but expect it to be the toughest obstacle yet.
+      
+      The duration of this round will be close to an hour, and upon its completion you will receive your certificates.
+      
+      The submissions will be thoroughly evaluated and the results will be announced on IEEE-SSIT's official instagram page the following day.`
     },
     {
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."  
+      heading: "Prizes",
+      text:`At the end of every rainbow we expect the fabled pot of gold, and every journey begins with the expectation of success at its end. Your Code 2 Clone journey is no different, so before you begin, let's see the treasure you will be on the quest to find!
+
+      With a total prize money pool of ₹7000, the respective podium finishes will recieve:
+      
+      ₹4000 for the Winner
+      ₹2000 for the Runner Up
+      ₹1000 for the Best-of-the-Rest
+      
+      In addition to that, all participants will recieve official certificates of appreciation by IEEE-SSIT for their participation with mention of the round progression they achieved in the event!`
     }
   ];
   
--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { VStack, Heading, Text } from "@chakra-ui/react";
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 function Timeline() {
   return (
@@ -10,13 +12,88 @@ function Timeline() {
         justifyContent={"space-evenly"}
         direction="column"
         m={"auto"}
-        h="100vh"
-        bg={"gray.900"}
+        minHeight={"100vh"}
+        bg={"blackAlpha.900"}
+        paddingBottom={"50px"}
+        py={20}
       >
-        <Heading as='h2' size='3xl'>Timeline</Heading>
-        <Text align={"center"} fontSize="xl">
-          Coming soon...
-        </Text>
+        <Heading as="h2" size="3xl">
+          Timeline
+        </Heading>
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(141,18,18)" }}
+            date="2PM - 3PM"
+            iconStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            //icon={<WorkIcon />}
+          >
+            <h1 className="vertical-timeline-element-title">
+              Round 1
+            </h1>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(141,18,18)" }}
+            date="3:45PM"
+            iconStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            //icon={<WorkIcon />}
+          >
+            <h1 className="vertical-timeline-element-title">
+              Round 1 Results
+            </h1>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(141,18,18)" }}
+            date="4PM - 5PM"
+            iconStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            //icon={<WorkIcon />}
+          >
+            <h1 className="vertical-timeline-element-title">
+              Round 2
+            </h1>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(141,18,18)" }}
+            date="5:45PM"
+            iconStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            //icon={<WorkIcon />}
+          >
+            <h1 className="vertical-timeline-element-title">
+              Round 2 Results
+            </h1>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(141,18,18)" }}
+            date="6PM - 7PM"
+            iconStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            //icon={<WorkIcon />}
+          >
+            <h1 className="vertical-timeline-element-title">
+              Round 3
+            </h1>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(141,18,18)" }}
+            date="October 1"
+            iconStyle={{ background: "rgb(141,18,18)", color: "#fff" }}
+            //icon={<WorkIcon />}
+          >
+            <h1 className="vertical-timeline-element-title">
+              Round 3 Results
+            </h1>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
       </VStack>
     </>
   );
